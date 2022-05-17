090e7e26da90d3a59d1439f37b554fbf1d3db4e5
rdegiovanni <rdegiovanni@dc.exa.unrc.edu.ar>
2022-05-17 14:07:28 +0200

    update in run-codebert.py script to get the masked sequence used to call codebert.

--- run-codebert.py
+++ run-codebert.py
@@ -35,13 +35,15 @@ tokenized_CODE = tokenizer.tokenize(CODE)
 tokens_count = str(len(tokenized_CODE))
 
 mask_index = tokenized_CODE.index("<mask>")
-start_index = max(0, mask_index - 256)
-stop_index = min(len(tokenized_CODE), mask_index + 256) - 1
-SHRINKED_TOKENS = tokenized_CODE[start_index:max(stop_index,511)]
-assert(len(SHRINKED_TOKENS)) #512 is the maximum sequence length for codebert model
+start_index = max(0, mask_index - 255)
+stop_index = min(len(tokenized_CODE), mask_index + 255) - 1
+SHRINKED_TOKENS = tokenized_CODE[start_index:max(stop_index,509)]
+#assert(len(SHRINKED_TOKENS)) #512 is the maximum sequence length for codebert model
 tokens_ids = tokenizer.convert_tokens_to_ids(SHRINKED_TOKENS)
 SHRINKED_CODE = tokenizer.decode(tokens_ids)
 
+SHRINKED_CODE_JSON = "{'masked_seq':" + repr(SHRINKED_CODE) + "}"
+print(SHRINKED_CODE_JSON)
 
 fill_mask = pipeline('fill-mask', model=model, tokenizer=tokenizer)
 outputs = fill_mask(SHRINKED_CODE)
